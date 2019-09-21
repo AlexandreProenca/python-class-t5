45c51058462602d94c73cb283a6645e5969bf4f6
Daniela <daniela@gmail.com>
2019-09-21 15:09:41 -0300

    SEGUNDO EXERCICIO

--- alunos/Daniela/exercicio2.py
+++ alunos/Daniela/exercicio2.py
@@ -4,5 +4,7 @@ nota2=int(input('Digite a segunda nota: '))
 nota3=int(input('Digite a terceira nota: '))
 nota4=int(input('Digite a quarta nota'))
 media=(nota1+nota2+nota3+nota4)/4
-print(f'{nome}sua média é{media}')
+
+
+
 
