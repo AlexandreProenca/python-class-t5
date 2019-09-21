d5c1a0b685c9bac55dec7658ae0442b872ee7fa6
nicollasgomes10 <43495092+nicollasgomes10@users.noreply.github.com>
2019-09-21 16:00:45 -0300

    Mais exemplos

--- aulas/aula02.py
+++ aulas/aula02.py
@@ -46,7 +46,14 @@ falso = False, 0,'', None
 
 
 # Função de leitura do teclado
-nome = input('Qual é o seu nome?') # para a execusao e aguarda ate que algum valor seja inputado
+nota1 = int(input('digite a primeira nota')) # para a execusao e aguarda ate que algum valor seja inputado
+nota2 = int(input('digite a primeira nota'))
+nota3 = int(input('digite a primeira nota'))
+nota4 = int(input('digite a primeira nota'))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f'{nome} sua media e {media}')
+#TUDO QUE VEM DO INPUT É STRINGnota1 = int(input('digite a primeira nota'))
 
 # Função para apresentar resuldado na tela do monitor
 
@@ -54,4 +61,32 @@ print(f'OI {nome}', nome_completo2, sep=';', end=' finalmente visivel')
 
 
 # Faça um programa que receba 4 notas o nome de uma aluno e imprima o nome e a media das notas
+
+# Estrutura de decisao
+
+
+#if <condicao logica>:
+#	bloco de codigo
+#	.
+#	.
+#	.
+#	.
+
+# Faça um programa que receba 4 notas o nome de uma aluno, e calcule a media, se a media for menor que 3 esta reprovado, se a media estiver entre 3 e 7 esta em recuperação se a media for maior que 7 o aluno esta aprovado.
+nota1 = int(input('digite a primeira nota')) # para a execusao e aguarda ate que algum valor seja inputado
+nota2 = int(input('digite a primeira nota'))
+nota3 = int(input('digite a primeira nota'))
+nota4 = int(input('digite a primeira nota'))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+
+if not media >= 7:
+	print('aluno reprovado')
+else:
+	print('aluno aprovado')
+
+print(f'{nome} sua media e {media}')
+
+
 
