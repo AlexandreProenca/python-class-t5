efa927403c3e5fc9b96140ad9b8c951a715c630d
Pedro <pedrohschulze@gmail.com>
2019-09-21 15:03:49 -0300

    Alteracoes na escrita

--- alunos/Pedro/Exercicio01.py
+++ alunos/Pedro/Exercicio01.py
@@ -1,17 +1,17 @@
-numerador_media = 0
 denominador_media = 4
 
+# numerador_media = 0
 # for index in range(denominador_media):
-#     var = int(input(f"Digite o {index + 1}º numero: "))
+#     var = int(input(f"Digite a {index + 1}ª nota: "))
 #     numerador_media = numerador_media + var
-# media = numerador_media/denominador_media
 
-var_1 = int(input("Digite o 1º numero: "))
-var_2 = int(input("Digite o 2º numero: "))
-var_3 = int(input("Digite o 3º numero: "))
-var_4 = int(input("Digite o 4º numero: "))
+var_1 = int(input("Digite a 1º nota: "))
+var_2 = int(input("Digite a 2º nota: "))
+var_3 = int(input("Digite a 3º nota: "))
+var_4 = int(input("Digite a 4º nota: "))
+numerador_media = var_1 + var_2 + var_3 + var_4
 
-media = (var_1 + var_2 + var_3 + var_4)/4
+media = numerador_media/denominador_media
 aluno = input("Digite o nome do aluno: ")
 
 print(f"A média aritimética das notas do {aluno} é: {media}")
