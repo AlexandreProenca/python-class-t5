f42763e1ee4e6904b715ffd97d12a652da30a3bd
Pedro <pedrohschulze@gmail.com>
2019-09-23 09:25:12 -0300

    Limpando arquivos desnecessários

--- alunos/Pedro/Exercicio04_Zip.py
+++
@@ -1,8 +0,0 @@
-var_1 = int(input("Digite o 1º valor: "))
-var_2 = int(input("Digite o 2º valor: "))
-var_3 = int(input("Digite o 3º valor: "))
-
-if var_1<var2<var3:
-
-
-print(f"A maior variável é {maior}")
