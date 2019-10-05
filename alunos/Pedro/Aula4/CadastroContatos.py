4dcdad58a8f7de1bd264176e11e374be36093e3c
Pedro <pedrohschulze@gmail.com>
2019-10-05 09:59:12 -0300

    Programa de cadastro de contatos

---
+++ alunos/Pedro/Aula4/CadastroContatos.py
@@ -0,0 +1,24 @@
+dict_contato = dict()
+print("1) Cadastrar contato\n"
+      "2) Listar contatos\n"
+      "3) Mostrar um contato específico\n"
+      "4) Excluir um contato\n"
+      "5) Sair")
+
+while(True):
+    comando = int(input("\nDigite a ação que você queira:"))
+    if comando == 1:
+        nome_contato = input("Digite o nome do contato a ser inserido: ")
+        info_contato = input("Digite as informacoes do contato inserido: ")
+        dict_contato[nome_contato] = info_contato
+    if comando == 2:
+        for contato in dict_contato:
+            print(contato)
+    if comando == 3:
+        nome_contato = input("Digite o contato cujas informacoes serao exibidas")
+        print(dict_contato[nome_contato])
+    if comando == 4:
+        nome_contato = input("Digite o nome do contato a ser deletado: ")
+        del dict_contato[nome_contato]
+    if comando == 5:
+        break
---
+++ alunos/Pedro/Aula4/dicionario.py
@@ -0,0 +1,16 @@
+d = {"Nome" : ["Pedro", "Fulano", "Ximbinha"],
+     "Nota" : [10, 5, 2]}
+
+e = dict([
+    ("Nome", ["Pedro", "Fulano", "Ximbinha"]),
+    ("Nota", [10, 5, 2])
+])
+
+f = dict(
+    Nome=["Pedro", "Fulano", "Ximbinha"],
+    Nota=[10, 5, 2]
+)
+
+print(d)
+print(e)
+print(f)
