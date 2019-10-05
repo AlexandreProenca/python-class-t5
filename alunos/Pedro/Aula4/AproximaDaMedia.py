30959c16a539e607bc187bad8bebefab960bfd53
Pedro <pedrohschulze@gmail.com>
2019-10-05 11:11:34 -0300

    Commitando via pycharm

---
+++ alunos/Pedro/Aula4/AproximaDaMedia.py
@@ -0,0 +1,18 @@
+qtd_numeros = int(input('Digite a quantidade de numeros a ser inserido: '))
+lista_numeros = []
+lista_diff = []
+dict_numeros = {}
+
+for index in range(qtd_numeros):
+    lista_numeros += [int(input(f'{index+1}º numero: '))]
+
+media = sum(lista_numeros)/qtd_numeros
+
+for numero in range(qtd_numeros):
+    lista_diff += [abs(numero - media)]
+    dict_numeros[abs(numero - media)] = numero
+
+menor_erro = min(lista_diff)
+num_prox = dict_numeros[menor_erro]
+
+print(f"O numero mais proximo da media é {num_prox} com erro absoluto {menor_erro}")
