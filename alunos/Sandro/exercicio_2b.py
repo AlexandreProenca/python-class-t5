e3ae2f65430995649be5d6311d521b765a001f86
saggi1161 <saggi1161@gmail.com>
2019-10-19 15:11:00 -0300

    adicionado logica dicionario e metodo de impressao programa finalizado e testado. Usado dicionario com classe para soluçao do problema OK

--- alunos/Sandro/exercicio_2b.py
+++
@@ -1,8 +0,0 @@
-class quadrado:
-    def __init__(self,lado):
-        self.l = lado
-q1=quadrado(10)
-print(q1.__dict__)
-q1.l=20
-print(q1.l)
-print(f'O lado do quadrado é {q1.l} e a area é {q1.l*q1.l}')
